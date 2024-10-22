df4bf39f746fec8339544690d9702dc92e5dbb90
Shane Martin <shane@gotvantage.com>
2024-10-22 09:42:01 -0400

    feat: set up main module using dotenv creds

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { processKanban } from './processKaban';
+import dotenv from 'dotenv';
 
-processKanban();
+import { checkIn } from './checkIn.js';
+import { processKanban } from './processKanban.js';
+
+dotenv.config();
+
+const main = async () => {
+  const { STEADY_EMAIL, STEADY_PASSWORD } = process.env;
+
+  if (typeof STEADY_EMAIL !== 'string' || typeof STEADY_PASSWORD !== 'string') {
+    throw new Error('Missing credentials in dotenv file');
+  }
+
+  const tasks = await processKanban();
+
+  await checkIn({
+    credentials: { email: STEADY_EMAIL, password: STEADY_PASSWORD },
+    tasks,
+  });
+};
+
+main();
